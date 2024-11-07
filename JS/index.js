52386c948f6cffb47f3cedd1498288846c965c4f
Kayque48 <kayquec158@gmail.com>
2024-11-07 13:09:38 -0300

    primeiro commit

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var nome = "Kayque", sobrenome = "Costa", nomeCompleto = nome + " "+ sobrenome, idade = 18, pessoa = nomeCompleto + " " + idade;
+
+document.getElementById("texto").innerHTML = pessoa;
