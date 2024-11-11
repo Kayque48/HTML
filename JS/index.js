acf16532fa97af0abd54a9252a375a3c52dca0db
Kayque48 <kayquec158@gmail.com>
2024-11-11 12:04:11 -0300

    Delete JS directory

--- JS/index.js
+++
@@ -1,3 +0,0 @@
-var nome = "Kayque", sobrenome = "Costa", nomeCompleto = nome + " "+ sobrenome, idade = 18, pessoa = nomeCompleto + " " + idade;
-
-document.getElementById("texto").innerHTML = pessoa;
